6dd23e1dec666655354ffce5f3fdf43e6b3372ec
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-06-19 15:53:21 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        aura: "#25c3b5",
+        charcoal: "#2f2f2f",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
